88ad210de5fce029b4a4c481136d1223cf8af4a2
sumitjr3 <103193051+sumitjr3@users.noreply.github.com>
2023-12-24 16:11:52 +0530

    animated continer added

--- lib/views/animated_container.dart
+++ lib/views/animated_container.dart
@@ -8,8 +8,44 @@ class animated_container extends StatefulWidget {
 }
 
 class _animated_containerState extends State<animated_container> {
+  double x_axis = 1;
+  double y_axis = 1;
+  double boxHeight = 100;
+  double boxWidth = 100;
+  var boxColor = Colors.amber[700];
+
+  void _movebox() {
+    setState(() {
+      x_axis = -1;
+      y_axis = -1;
+    });
+  }
+
+  void _changeBoxColor() {
+    setState(() {
+      boxColor = Colors.blue[600];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GestureDetector(
+      onTap: _movebox,
+      onDoubleTap: _changeBoxColor,
+      child: Scaffold(
+        backgroundColor: Colors.amber[300],
+        body: Center(
+          child: AnimatedContainer(
+            duration: const Duration(seconds: 2),
+            alignment: Alignment(x_axis, y_axis),
+            child: Container(
+              width: boxWidth,
+              height: boxHeight,
+              color: boxColor,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
